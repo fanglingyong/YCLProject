d009242725f7e482ea85ce443939bab2da53d52c
zuozuoyou <1121927879@qq.com>
2017-11-10 16:21:46 +0800

    UI合并

---
+++ HJC/View/ShoppingCartCell/ShoppingCartFooterView.m
@@ -0,0 +1,31 @@
+//
+//  ShoppingCartFooterView.m
+//  HJC
+//
+//  Created by zhifu360 on 2017/11/10.
+//  Copyright © 2017年 EastChina. All rights reserved.
+//
+
+#import "ShoppingCartFooterView.h"
+
+@interface ShoppingCartFooterView()
+
+@property (nonatomic, retain)UIImageView *selectImageView;
+@property (nonatomic, retain)UILabel *allSelectLabel;
+
+@property (nonatomic, retain)UILabel *allMeterLB;
+@property (nonatomic, retain)UILabel *canUseLabel;
+
+@end
+
+@implementation ShoppingCartFooterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
